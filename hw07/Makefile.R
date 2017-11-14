3a031f5f6fbc7fdad2b6360e9750f4fc3984d9c5
Xinmiao <xinmiaow@sfu.ca>
2017-11-14 03:53:24 -0800

    comment `setwd` out

--- hw07/Makefile.R
+++ hw07/Makefile.R
@@ -9,7 +9,7 @@ outputs <- c("gapminder.tsv",              # 0_download-data.R
 file.remove(outputs)
 
 ## run my scripts
-setwd("C:/Users/ThinkPad/Desktop/2017 Fall/STAT545A/STAT545-hw-Wang-Xinmiao/hw07")
+#setwd("C:/Users/ThinkPad/Desktop/2017 Fall/STAT545A/STAT545-hw-Wang-Xinmiao/hw07")
 source("0_download_data.R")
 source("1_exploratory_analyses.R")
 source("2_statistical_analyses.R")
