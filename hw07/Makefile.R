3e8418e6c703d105f8f1d08b9492b63ab0fe1ac3
Xinmiao <xinmiaow@sfu.ca>
2017-11-14 01:57:02 -0800

    have trouble to remove figures

--- hw07/Makefile.R
+++ hw07/Makefile.R
@@ -1,15 +1,16 @@
 ## one script to rule them all
 
 ## clean out any previous work
-outputs <- c("./hw07/gapminder.tsv",              # 0_download-data.R
-             "./hw07/gap_re_continent.rds",       # 1_exploratory_analyses.R
-             "./hw07/lm_fits.tsv",                # 2_statistical_analyses.R
-             "./hw07/gap_bfit.tsv",               # 2_statistical_analyses.R
-             list.files(pattern = "*.png$"))      # Figures created by all R Scripts
-file.remove(outputs)
+outputs <- c("gapminder.tsv",              # 0_download-data.R
+             "gap_re_continent.rds",       # 1_exploratory_analyses.R
+             "lm_fits.tsv",                # 2_statistical_analyses.R
+             "gap_bfit.tsv",               # 2_statistical_analyses.R
+             list.files(path="./hw07/",pattern = "*.png$"))      # Figures created by all R Scripts
+file.remove(path="./hw07/", outputs)
 
 ## run my scripts
 source("./hw07/0_download_data.R")
 source("./hw07/1_exploratory_analyses.R")
 source("./hw07/2_statistical_analyses.R")
 source("./hw07/3_generate_figures.R")
+
