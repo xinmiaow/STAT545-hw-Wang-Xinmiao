5ef1721bc37fdbb4efecdb0af756c96621845c8f
Xinmiao <xinmiaow@sfu.ca>
2017-11-14 00:45:22 -0800

    create a R Script called Makefile

---
+++ hw07/Makefile.R
@@ -0,0 +1,15 @@
+## one script to rule them all
+
+## clean out any previous work
+outputs <- c("gapminder.tsv",                     # 0_download-data.R
+             "gap_re_continent.rds",              # 1_exploratory_analyses.R
+             "lm_fits.tsv",                       # 2_statistical_analyses.R
+             "gap_bfit.tsv",                      # 2_statistical_analyses.R
+             list.files(pattern = "*.png$"))
+file.remove(outputs)
+
+## run my scripts
+source("0_download-data.R")
+source("1_exploratory_analyses.R")
+source("2_statistical_analyses.R")
+source("3_generate_figures")
