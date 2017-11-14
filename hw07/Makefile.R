91a65cf693db4a9c3be63fce6515a1d94d5129a2
Xinmiao <xinmiaow@sfu.ca>
2017-11-14 00:47:07 -0800

    add comments

--- hw07/Makefile.R
+++ hw07/Makefile.R
@@ -5,7 +5,7 @@ outputs <- c("gapminder.tsv",                     # 0_download-data.R
              "gap_re_continent.rds",              # 1_exploratory_analyses.R
              "lm_fits.tsv",                       # 2_statistical_analyses.R
              "gap_bfit.tsv",                      # 2_statistical_analyses.R
-             list.files(pattern = "*.png$"))
+             list.files(pattern = "*.png$"))      # Figures created by all R Scripts
 file.remove(outputs)
 
 ## run my scripts
